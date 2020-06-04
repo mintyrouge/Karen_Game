1425d7c34a390ab923f7c347f7cc3425b96977da
mishakov7 <astaffers@gmail.com>
2020-06-03 23:03:28 -0400

    Created new scenes and linked some main menu buttons.

---
+++ Assets/Script/SwitchScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public int sceneIdx;
+
+    // Start is called before the first frame update
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: sceneIdx);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
