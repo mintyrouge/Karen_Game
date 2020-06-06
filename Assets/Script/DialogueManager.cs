75bdb693188821fc062878349f582ef806fb695c
mishakov7 <astaffers@gmail.com>
2020-06-06 13:43:37 -0400

    Started out the UI for a basic dialogue scene.

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    // Works like a list but is more restricted (uses FIFO data structure)
+    private Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+}
