4274c5f0d93b03e181927111a5be740f8c73f7b7
Karen P <48729437+mintyrouge@users.noreply.github.com>
2020-06-14 20:34:46 -0400

    Added SceneSwitcher Script
    Also:
    blacked out the unplayable areas

---
+++ Assets/Script/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+   
+    public void GotoGarbucks()
+    {
+        SceneManager.LoadScene("GarbucksAltercation");
+    }
+}
