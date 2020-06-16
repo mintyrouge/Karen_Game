13eb6b068b7fc66474721c39438c51842680aaf1
mishakov7 <astaffers@gmail.com>
2020-06-16 19:18:27 -0400

    Player selection is functional now. I used a test scene, though.

---
+++ Assets/Script/CharacterPersist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CharacterPersist : MonoBehaviour
+{
+    public TMP_Text playerText;
+    public Image playerImage;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log(Globals.chosenPlayer.name);
+        playerText.text = "You picked " + Globals.chosenPlayer.name;
+        playerImage.sprite = Globals.chosenPlayer.image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/CharacterSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Globals {
+    public static Character chosenPlayer;
+}
+
+public class CharacterSelect : MonoBehaviour {
+
+    public void SelectPlayer(Character player) {
+        Globals.chosenPlayer = player;
+        SceneManager.LoadScene(4);
+    }
+
+    void Start() { }
+    void Update() { }
+}
