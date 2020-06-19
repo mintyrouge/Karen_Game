285be11230b93c43e6b4e8dc679826a211fff91e
mishakov7 <astaffers@gmail.com>
2020-06-19 01:12:34 -0400

    Added notifications to the AltercationTest.scene

---
+++ Assets/Script/Notification.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Notification : MonoBehaviour
+{
+    public GameObject entry;
+    public GameObject fail;
+    public GameObject win;
+
+    public void EnterStore() {
+        entry.SetActive(false);
+    }
+
+    public void ShowNotification(GameObject notif) {
+        notif.SetActive(true);
+    }
+
+    public void LeaveStore() {
+        SceneManager.LoadScene(0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
